71c09713491ec1b7dcc9a55a71a044305dca11b1
Grigory Ptashko <grigory.ptashko@gmail.com>
2016-02-18 00:52:17 +0300

    Updated unit-tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -31,6 +31,22 @@ describe(@"Tests", ^{
 
         expect(p.cyclesCount).to.equal(2);
     });
+
+    it(@"can track time of one cycle", ^{
+        GPTimeProfiler *p = [[GPTimeProfiler alloc] init:@"name"];
+        [p start];
+        [NSThread sleepForTimeInterval:1.0f];
+        [p stop];
+
+        expect(p.totalTime).to.to.beGreaterThan(1.0f);
+    });
+
+    it(@"can have NULL name of the profiler", ^{
+        GPTimeProfiler *p = [[GPTimeProfiler alloc] init:NULL];
+        [p start];
+
+        [p stop];
+    });
 });
 
 SpecEnd
